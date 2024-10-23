87f57d38e464db841067f6c3b0100b72c36529b1
Siddardhmustplay <siddumustplaygames@gmail.com>
2024-10-23 12:33:07 +0530

    third

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Home.css';
 import friends from './images/friends.jpg';
 
 const Home = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  // Extract username from query parameters
+  const queryParams = new URLSearchParams(location.search);
+  const currentUsername = queryParams.get('username') || 'Guest'; // Default to 'Guest' if not found
 
   const quizzes = [
     { type: 'friend1', path: '/question' },
@@ -19,13 +24,19 @@ const Home = () => {
   };
 
   return (
-    <div>
+    <div className="home-container">
+      <div className="coins-button-container">
+        <button className="btn btn-dark coins-button">Coins: 100</button>
+      </div>
+      <div className='text-center'>
+        <h2>Welcome, {currentUsername}</h2> {/* Display the username */}
+      </div>
       {quizzes.map((quiz, index) => (
         <section key={index} className="d-flex align-items-center justify-content-center">
           <div className="container">
             <div className="row justify-content-center">
               <div className='col-md-12 text-center'>
-                <h1 className='protest-strike-regular-head text-center'>Know Your Friend</h1>
+                <h1 className='protest-strike-regular-head'>Know Your Friend</h1>
                 <div>
                   <img src={friends} className='frnd-png' alt="Friends" />
                 </div>
